54c8bbed959768d0e5bf9cfa3a22496575dae532
Mahir Hasanli <126595932+MAHIRHASANLI@users.noreply.github.com>
2025-01-15 19:49:07 +0400

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,7 @@ const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   style: ["normal", "italic"],
   subsets: ["latin"],
+  display: "swap",
 });
 
 export default function RootLayout({
